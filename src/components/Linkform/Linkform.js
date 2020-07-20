52be32565ccc82d378af7d1b2a3fcf01f755caba
Vimean Chea <vimeanc@csu.fullerton.edu>
2020-07-20 10:15:18 -0700

    clean up

--- src/components/Linkform/Linkform.js
+++ src/components/Linkform/Linkform.js
@@ -1,7 +1,21 @@
 import React from "react";
+import './Linkform.css';
 
-export default function Linkform(){
+export default function Linkform({onInputChange, onButtonSubmit}){
 return(
-    <p>Submt</p>
+    <div>
+      <p className='f3'>
+        {'This Magic Brain will detect faces in your pictures. Git it a try.'}
+      </p>
+      <div className='center'>
+        <div className='form center pa4 br3 shadow-5'>
+          <input className='f4 pa2 w-70 center' type='tex' onChange={onInputChange}/>
+          <button
+            className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
+            onClick={onButtonSubmit}
+          >Detect</button>
+        </div>
+      </div>
+    </div>
 )
 }
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,13 +1,18 @@
 import React from "react";
 import Tilt from 'react-tilt';
 import "./Logo.css";
-import image from "./logo.png";
+import image from "./image.png";
+import "tachyons";
 
 export default function Logo(){
     return(
-<Tilt className="Tilt" options={{ max : 25 }} style={{ height: 150, width: 250 }} >
- <div className="Tilt-inner"> <img alt="logo" src ={image} size= "200" /> </div>
-</Tilt>
+        <div className='ma4 mt0'>
+        <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+          <div className="Tilt-inner pa3">
+            <img style={{paddingTop: '5px'}} alt='logo' src={image}/>
+          </div>
+        </Tilt>
+      </div>
 )
 }
 
